7c6b6f76637222a3ca040accf67d1528705bc8a1
isvitality <80138585+isvitality@users.noreply.github.com>
2025-10-27 13:17:56 -0500

    feat: Increase logo size in Header and Footer
    Enlarges the `Logo` component in both the `Header` and `Footer` components to improve brand visibility and visual impact.

--- components/Header.tsx
+++ components/Header.tsx
@@ -101,7 +101,7 @@ const Header: React.FC = () => {
             animate={{ opacity: 1 }}
             transition={{ duration: 0.5 }}
           >
-            <Logo className="h-12" />
+            <Logo className="h-36" />
           </motion.a>
         </div>
         
